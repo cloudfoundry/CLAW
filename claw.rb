fb8fa0dd27c4fcaf2db8cb050503fd60282b5f08
Christopher Piraino and David Varvel <cpiraino@pivotallabs.com>
2014-07-22 12:10:57 -0700

    Add 6.3.1 to the CLAW

--- claw.rb
+++ claw.rb
@@ -17,6 +17,7 @@ AVAILABLE_VERSIONS = %w{
   6.1.2
   6.2.0
   6.3.0
+  6.3.1
 }
 STABLE_VERSION = AVAILABLE_VERSIONS.last
 VERSIONED_RELEASE_LINK = 'http://go-cli.s3-website-us-east-1.amazonaws.com/releases/v%{version}/%{release}'

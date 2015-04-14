bf862a1e1c52d4a657722aba99c2bdf50c391a12
Jonathan Berkhahn <jaberkha@us.ibm.com>
2015-04-14 10:18:50 -0700

    Bump version 6.11.0

--- claw.rb
+++ claw.rb
@@ -29,6 +29,7 @@ AVAILABLE_VERSIONS = %w{
   6.8.0
   6.9.0
   6.10.0
+  6.11.0
 }
 STABLE_VERSION = AVAILABLE_VERSIONS.last
 VERSIONED_RELEASE_LINK = 'http://go-cli.s3-website-us-east-1.amazonaws.com/releases/v%{version}/%{release}'

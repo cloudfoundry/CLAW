ee441c256819c4a9dd61cf43f9832711e1a9f1b9
Simon Leung <leungs@us.ibm.com>
2015-07-06 17:41:20 -0700

    Bump to 6.12.1

--- claw.rb
+++ claw.rb
@@ -34,6 +34,7 @@ AVAILABLE_VERSIONS = %w{
   6.11.2
   6.11.3
   6.12.0
+  6.12.1
 }
 STABLE_VERSION = AVAILABLE_VERSIONS.last
 VERSIONED_RELEASE_LINK = 'http://go-cli.s3-website-us-east-1.amazonaws.com/releases/v%{version}/%{release}'

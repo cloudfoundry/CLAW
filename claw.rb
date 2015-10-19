dab805d03adb140e4cf7765bbdb576fba87b185f
Kris Hicks <khicks@pivotal.io>
2015-10-19 14:59:13 -0700

    Add 6.13.0

--- claw.rb
+++ claw.rb
@@ -38,6 +38,7 @@ AVAILABLE_VERSIONS = %w{
   6.12.2
   6.12.3
   6.12.4
+  6.13.0
 }
 STABLE_VERSION = AVAILABLE_VERSIONS.last
 VERSIONED_RELEASE_LINK = 'http://go-cli.s3-website-us-east-1.amazonaws.com/releases/v%{version}/%{release}'

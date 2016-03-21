440e7dc50f349643204c39d8a0b8228f71de8a7e
Corbin Halliwill <challiwill@pivotal.io>
2016-03-21 16:11:07 -0700

    Add $ to variable names
    Ca$h rule$ everything around me...
    [#116024861]
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- ci/scripts/run-deploy.sh
+++ ci/scripts/run-deploy.sh
@@ -4,5 +4,5 @@ curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github-
 tar -xzf cf.tgz
 
 cd claw
-../cf login -a PWS_API -u PWS_USERNAME -p PWS_PASSWORD -o PWS_ORG -s PWS_SPACE
+../cf login -a $PWS_API -u $PWS_USERNAME -p $PWS_PASSWORD -o $PWS_ORG -s $PWS_SPACE
 ../cf push -f manifest.yml

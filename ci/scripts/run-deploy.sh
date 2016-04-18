c6c50e15f83e5659677312329dc07982072239ad
Chris Hendrix <chendrix@pivotal.io>
2016-04-18 12:29:37 -0700

    Use cf-resource to deploy to PWS
    [finishes #105867654]
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- ci/scripts/run-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github-rel" > cf.tgz
-tar -xzf cf.tgz
-
-cd claw
-../cf login -a $PWS_API -u $PWS_USERNAME -p $PWS_PASSWORD -o $PWS_ORG -s $PWS_SPACE
-../cf push -f manifest.yml

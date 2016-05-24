960b733969804267d309b60fa5096874ab7077cf
Corbin Halliwill <challiwill@pivotal.io>
2016-05-24 14:00:32 -0700

    Release 6.18.1
    [#119237305]
    
    Signed-off-by: Corbin Halliwill <challiwill@pivotal.io>

--- claw.rb
+++ claw.rb
@@ -62,6 +62,7 @@ AVAILABLE_VERSIONS = %w{
   6.17.0
   6.17.1
   6.18.0
+  6.18.1
 }
 STABLE_VERSION = AVAILABLE_VERSIONS.last
 VERSIONED_RELEASE_LINK = 'https://s3.amazonaws.com/go-cli/releases/v%{version}/%{release}'

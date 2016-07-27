ecbf5716cb544e2b5e3d5c6d409266ba526ed969
Nick Wei <weijin84@gmail.com>
2016-07-27 10:14:06 -0700

    Update claw.rb
    Bump CLI version.

--- claw.rb
+++ claw.rb
@@ -65,6 +65,7 @@ AVAILABLE_VERSIONS = %w{
   6.18.1
   6.19.0
   6.20.0
+  6.21.0
 }
 STABLE_VERSION = AVAILABLE_VERSIONS.last
 VERSIONED_RELEASE_LINK = 'https://s3.amazonaws.com/go-cli/releases/v%{version}/%{release}'
